95c40d14b06d8bf2c2c7878cba91ce4f3460bcd1
TEJASWI MALINENI <tejaswimalineni2005@gmail.com>
2026-05-30 22:16:01 +0530

    Day 4 - Added Reverse Array solution

---
+++ ReverseArray.java
@@ -0,0 +1,35 @@
+Array ni reverse cheyyali.
+  Input:  [1, 2, 3, 4, 5]
+Output: [5, 4, 3, 2, 1]
+
+
+  
+import java.util.Arrays;
+
+public class ReverseArray {
+
+    public static void reverse(int[] arr) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+
+            left++;
+            right--;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5};
+
+        reverse(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
